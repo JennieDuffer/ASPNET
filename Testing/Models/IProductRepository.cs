ce9aacc9ff5ac854112453a83e0e4d238db2e6e5
Jennie Duffer <jenniej297@gmail.com>
2022-09-19 11:57:10 -0500

    create product

--- Testing/Models/IProductRepository.cs
+++ Testing/Models/IProductRepository.cs
@@ -5,8 +5,15 @@ namespace Testing.Models
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+
         public Product GetProduct(int id);
 
         public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
     }
 }
